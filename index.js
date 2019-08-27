f5363753ae43244030eb72aa9a38c3ec85115a43
Steve <stewright88@gmail.com>
2019-08-27 16:10:28 +0200

    almost done, just needs countdown timer and guess count

--- index.js
+++ index.js
@@ -1,85 +1,59 @@
-// card data
-const cardsArray = [{
-        name: 'pikachu',
-        img: './images/pikachu.png',
-    },
-    {
-        name: 'onix',
-        img: './images/onix.png',
-    },
-    {
-        name: 'mewtwo',
-        img: './images/mewtwo.png',
-    },
-    {
-        name: 'mew',
-        img: './images/mew.png',
-    },
-    {
-        name: 'machop',
-        img: './images/machop.png',
-    },
-    {
-        name: 'eevee',
-        img: './images/eevee.png',
-    },
-    {
-        name: 'cubone',
-        img: './images/cubone.png',
-    },
-    {
-        name: 'abra',
-        img: './images/abra.png',
-    },
-    {
-        name: 'charmander',
-        img: './images/charmander.png',
-    },
-    {
-        name: 'bulbasaur',
-        img: './images/bulbasaur.png',
-    },
-    {
-        name: 'snorlax',
-        img: './images/snorlax.png',
-    },
-    {
-        name: 'squirtle',
-        img: './images/squirtle.png',
-    },
-]
+const cards = document.querySelectorAll('.card');
 
-const game = document.querySelector('.game')
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-const grid = document.createElement('section') //var element = document.createElement(tagName)
-grid.classList.add('grid')
-game.appendChild(grid) //node.appendChild(node) places the grid section withing the game section
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-let gameGrid = cardsArray.concat(cardsArray) // this makes an array of cardsArray * 2
-gameGrid.sort(() => 0.5 - Math.random()) // not 100% sure what all this does but it's randomizing the cards every page refresh
+    this.classList.add('flip');
 
-gameGrid.forEach(function (item) { // this loops through the gamesGrid array and creates a div for everyone and assigns it the class of card
-    const card = document.createElement('div')
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
 
-    card.classList.add('card')
-    // card.dataset.name = item.name
-    card.style.backgroundImage = `url(${item.img})`
-
-    grid.appendChild(card) // this places the card divs within the grid section
-})
-
-let firstGuess = ''
-let secondGuess = ''
-let count = 0
-
-grid.addEventListener('click', function (event) {
-    let clicked = event.target
-
-    if (clicked === '.card') {
-        return true;
+        return;
     }
-    if (count < 2) {
-        count++
-        clicked.classList.add('selected')
-    }
-})
+
+    secondCard = this;
+    matched();
+}
+
+function matched() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 800);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 24);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
