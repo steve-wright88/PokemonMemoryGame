6daaa5cadc2e19ec58b59c2b5892df619834e1ea
Steve <stewright88@gmail.com>
2019-08-28 17:46:34 +0200

    complete, everything wokring fine

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const cards = document.querySelectorAll('.card');
+document.querySelector(".counter").onclick = () => {
+    console.log("test")
+    document.querySelectorAll('.card').forEach((card) => card.classList.add("flip"))
+}
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -16,32 +20,45 @@ function flipCard() {
 
         return;
     }
-
     secondCard = this;
     matched();
 }
 
-function matched() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+let guessCounter = 0
+var isFinished = false
 
+function matched() {
+
+    guessCounter += 1
+    document.querySelector('.counter').innerText = guessCounter;
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCards() : unflipCards();
+
+    if (isMatch === true) {
+
+        document.querySelectorAll('.card.flip').forEach((card) => card.style.visibility = "hidden")
+        if (document.querySelectorAll('.flip').length === 24) {
+            document.getElementById('gameWrapper').innerHTML = `<div class="wrapper"><h2 class="win">CONGRATULATIONS!!</h2><a class="btn-win" href="game.html">Play Again</a></div>`
+            isFinished = true
+            timeleft = "Finished"
+
+        }
+    }
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-
     resetBoard();
 }
 
 function unflipCards() {
     lockBoard = true;
-
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         resetBoard();
-    }, 800);
+    }, 600);
 }
 
 function resetBoard() {
@@ -57,3 +74,31 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+// #####################################################################################################
+var timeleft = 5;
+var startGame = true;
+
+document.addEventListener('click', function (event) {
+    if (startGame === true) {
+        var downloadTimer = setInterval(function () {
+            document.getElementById("countdown").innerHTML = timeleft;
+            if (isFinished === false) {
+                timeleft -= 1;
+                if (timeleft <= 0) {
+                    clearInterval(downloadTimer);
+                    document.getElementById("countdown").innerHTML = "Finished"
+                    document.getElementById('gameWrapper').innerHTML = `<div class="wrapper"><h2 class="over">GAME OVER!!</h2><a class="btn-over" href="game.html">Try Again</a></div>`
+                }
+            }
+        }, 1000);
+        startGame = false;
+    }
+}, false);
+
+let clickCount = document.getElementsByClassName(".counter"),
+    count = 0;
+clickCount.unflipCards() = function () {
+    count += 1;
+    clickCount.innerTEXT = count;
+}
